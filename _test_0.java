a1cc5e8e1815c73efea7d3d4cd5ab76b954bf112
Ronnie <ronnie94official@gmail.com>
2024-06-03 10:58:46 +0100

    Update _test_0.java+++

--- _test_0.java
+++ _test_0.java
@@ -14,7 +14,8 @@ public class CorsConfig {
   
   @Bean
   public WebMvcConfigurer corsConfigurer() {
-    
+
+  
     return new WebMvcConfigurer() {
       @Override
       public void addCorsMappings(CorsRegistry registry) {
