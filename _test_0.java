a53a01e6d64a8c3985543cb6469baee99ad88bde
Ronnie <ronnie94official@gmail.com>
2024-06-03 10:57:50 +0100

    Update _test_0.java

--- _test_0.java
+++ _test_0.java
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig {
-  
+ 
   @Value("${server.cors.allowedOrigins}")
   private String[] allowedOrigins;
   
