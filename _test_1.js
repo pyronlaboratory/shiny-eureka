a9a2d28af1e7dabdd403569c58bce17dd6cf3b7b
Ronnie <ronnie94official@gmail.com>
2024-06-21 18:13:25 +0100

    Update _test_0.java+++ (#15)
    * Update _test_0.java
    
    * Update _test_1.js
    
    * Added comments to 6 functions across 2 files
    
    ---------
    
    Co-authored-by: komment-ai-beta[bot] <146334705+komment-ai-beta[bot]@users.noreply.github.com>

--- _test_1.js
+++ _test_1.js
@@ -1,3 +1,20 @@
+/**
+ * @description Checks if a given value `x` is present in an array `arr` within a
+ * given range `start` and `end`. It returns `true` if `x` is found, or `false` otherwise.
+ * 
+ * @param { array } arr - array that contains the values to be searched for the
+ * specified `x` value.
+ * 
+ * @param { integer } x - value being searched for in the array.
+ * 
+ * @param { number } start - index of the leftmost element in the array that should
+ * be searched for the specified value `x`.
+ * 
+ * @param { number } end - 2nd index of the array where the target element `x` is located.
+ * 
+ * @returns { boolean } a boolean value indicating whether the provided element is
+ * present in the array within the specified range.
+ */
 const search = (arr, x, start, end) => {
   if (start > end) return false;
   let mid = Math.floor((start + end) / 2);
@@ -8,7 +25,12 @@ const search = (arr, x, start, end) => {
     return search(arr, x, mid + 1, end);
   }
 };
-
+/**
+ * @description Retrieves the application ID based on the input parameter "sysparm_appName"
+ * or "sysparm_appID". If the input is not provided, it returns an empty string.
+ * 
+ * @returns { integer } a unique identifier for the specified application.
+ */
 const getApplicationID = () => {
   var appID = "";
   gs.log("appid: " + this.getParameter("sysparm_appName"), "pipeline");
@@ -20,6 +42,17 @@ const getApplicationID = () => {
  return appID;
 }
 
+/**
+ * @description Takes an array of cells as input, loops through each row, and calculates
+ * the number of alive neighbors for each cell. Based on these calculations, it assigns
+ * a value to each cell in the next generation.
+ * 
+ * @param { array } cells - 2D array of cells that will be used to generate the next
+ * generation.
+ * 
+ * @returns { array } an array of booleans representing the alive cells in the next
+ * generation.
+ */
 function newGeneration(cells) {
   const nextGeneration = []
   for (let i = 0; i < cells.length; i++) {
